c9456a92e26beacf6ca1d51fe393580ac5574bf3
yoqub99 <nurmamatovyoqub099@gmail.com>
2021-09-21 01:58:25 +0500

    coment

---
+++ precache-manifest.8632818809442a7df6c056520111af8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aee8bbbef931be861dcf1ff5f46b350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17febc310324ad0cf6bc",
+    "url": "/static/css/main.8b421d16.chunk.css"
+  },
+  {
+    "revision": "bcbaf69e87362736ec6f",
+    "url": "/static/js/2.8fd13db8.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/static/js/2.8fd13db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17febc310324ad0cf6bc",
+    "url": "/static/js/main.eca53ddb.chunk.js"
+  },
+  {
+    "revision": "723b8de4144d7b4aea6d",
+    "url": "/static/js/runtime-main.562f75cf.js"
+  }
+]);
